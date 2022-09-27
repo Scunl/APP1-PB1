67e86e5ed6b6dcb3c9d08b86030fe23a3bb17204
Testavecdiscord <walid.amara.pro@gmail.com>
2022-09-27 02:10:43 +0200

    Marché européen... finito

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
+import argparse
+from filecmp import cmp
 from fltk import attend_fermeture, cree_fenetre, mise_a_jour, rectangle
 
 def traitement(file):
@@ -8,25 +9,20 @@ def traitement(file):
     f = open(file, "r")
     liste = f.readlines()
     liste = totext(liste)
-    f.close()
     return liste
 
 
-def check(file: str) -> bool:
+def check(file):
     """
     """
-    cmp = 0 
-    liste = traitement(file)
-    liste = backslash(liste)
+    cmp = 0
+    file = backslash(liste)
     while liste[cmp] == "":
         cmp+=1
-    return liste[cmp]
+
+    return str(liste[cmp])
     
-def binaire(liste):
-    liste2 = []
-    for i in range(2, len(liste)):
-        liste2.append(liste[i])
-    return liste2
+    
 
 def totext(liste):
     """
@@ -37,6 +33,7 @@ def totext(liste):
             liste2.append(commentaire(elem))
         else:
             liste2.append(elem)
+            
     return liste2
 
 
@@ -63,33 +60,35 @@ def couple(liste):
     return liste[0], liste[1], "".join(liste[2:])
 
 def backslash(liste):
+    
     return list(map(str.rstrip, liste)) 
 
 
-def parametrep1(liste: list) -> tuple:
+def parametre(liste, type):
+    '''
+    '''
     pixels = []
-    px = liste[0]
     format = liste[1]
-    for i in range(3, len(liste)):
-        pixels.append(liste[i])
-    return (px, format, pixels)
+    if type == "P3":
 
-def parametrep3(liste: list) -> tuple:
-    '''
-    '''
-    pixels = []
-    format = liste[1]
-    valmax = int(liste[2])
-    px = liste[0]
-    for i in range(3, len(liste)):
-        pixels.append(liste[i])
-    return (format, valmax, pixels)
+        valmax = int(liste[2])
+        for i in range(3, len(liste)):
+            pixels.append(liste[i])
+        return (format, valmax, pixels)
+
+    if type == "P1":
+        for i in range(2, len(liste)):
+            pixels.append(liste[i])
+        
+        return pixels, format
+    
 
 def forme(format):
     liste = []
     format2 = format.split(" ")
     for elem in format2:
         liste.append(int(elem))
+        
     return tuple(liste)
 
 
@@ -97,6 +96,7 @@ def separation(pixels):
     liste = []
     for elem in pixels:
         liste.append([elem])
+    
     return liste
 
 
@@ -111,9 +111,19 @@ def strtoint(pixels):
     liste = []
     for elem in pixels:
         for caractere in elem:
-            if caractere != ' ':
+            if len(caractere) > 0:
                 liste.append(int(caractere))
     return liste
+
+
+def strtoint1(pixels):
+    liste = []
+    for elem in pixels:
+        for caractere in elem:
+            if caractere != " ":
+                liste.append(int(caractere))
+    return liste
+
 
 def totuple(liste):
     """
@@ -123,10 +133,9 @@ def totuple(liste):
         liste2.append((liste[i-3], liste[i-2], liste[i-1]))
     return liste2
 
-def produit(liste):
+def produit(valmax, liste):
     """
     """
-    valmax = parametrep3(liste)
     liste2 = []
     for i, elem in enumerate(liste):
         liste2.append([])
@@ -139,18 +148,15 @@ def produit(liste):
 def convertion(liste):
     """
     """
-    liste2 = list()
-    for i, elem in enumerate(liste):
-        liste2.append([])
-        for tupl in elem:
-            liste2[i].append(hex(tupl)[2:])
+    liste2 = []
+    for i in range(len(liste)):
+        liste2.append(''.join('{:02X}'.format(a) for a in liste[i]))
     return liste2
+
 
 def addition(liste):
     '''
     '''
-    print(liste)
-    liste = convertion(liste)
     liste2 = []
     for i, seq in enumerate(liste):
         liste2.append([])
@@ -161,112 +167,119 @@ def addition(liste):
                 liste2[i].append(elem)
     return liste2
 
+
 def addition2(liste):
     '''
     '''
-    liste = addition(liste)
     liste2 = []
     for i in range(len(liste)):
         liste2.append(''.join(liste[i]))
     return liste2
 
-def lignep3(x, y, liste, taille=1):
+
+def ligne(x, y, liste, miseajour=False, taille=1):
     """
     """
     cmpt = 0
-    for j in range(x):
-        for i in range(y):
-            rectangle((i*taille), (j*taille), ((i+1)*taille) ,((j+1)*taille), remplissage='#' + str(liste[cmpt]), epaisseur=0)
+    for i in range(y):
+        for j in range(x):
+            rectangle((j*taille), (i*taille), ((j+1)*taille) ,((i+1)*taille), remplissage='#' + str(liste[cmpt]), epaisseur=0)
             cmpt += 1
+            if miseajour:
+                mise_a_jour()
     return None
 
-def lignep1(x, y, liste, taille=1):
+def ligne1(x, y, liste, miseajour=False, taille=1):
+    """
+    """
     cmpt = 0
-    for i in range(x):
-        for j in range(y):
-            
-            if liste[x]:
-                rectangle((j*taille), (i*taille), ((j+1)*taille) ,((i+1)*taille), remplissage='Black', epaisseur=0)
-            else:
+    for i in range(y):
+        for j in range(x):
+            if liste[cmpt] == 0:
                 rectangle((j*taille), (i*taille), ((j+1)*taille) ,((i+1)*taille), remplissage='White', epaisseur=0)
-            cmpt += 1
-    
-    return None
-
-def traitementp1(liste, taille=1):
-    a, b = liste[0], liste[1]
-    a = b.split(" ")
-    liste2 = []
-    for elem in a:
-        liste2.append(int(elem))
-    cree_fenetre(liste2[0]*taille, liste2[1]*taille)
-    pixels = binaire(liste)
-    pixels = strtoint(pixels)
-
-    return a, b, pixels, liste2
-
-
-def fonctiontraitementp1(liste, taille=1):
-    file = "P1-P6\guybrush3.p3.ppm"
-    liste = traitement(file)
-    liste = totext(liste)
-    liste = suppression(liste)
-    liste = backslash(liste)
-    a, b, pixels, liste2 = traitementp1(liste, taille)
-    cmpt = 0
-    for i in range(liste2[1]):
-        for j in range(liste2[0]):
-            if pixels[cmpt]:
-                rectangle((j*taille), (i*taille), (j+1)*taille, (i+1)*taille, remplissage='Black', epaisseur=0)
             else:
-                rectangle((j*taille), (i*taille), (j+1)*taille, (i+1)*taille, remplissage='White', epaisseur=0)
+                rectangle((j*taille), (i*taille), ((j+1)*taille) ,((i+1)*taille), remplissage='Black', epaisseur=0)
             cmpt += 1
+            if miseajour:
+                mise_a_jour()
     return None
 
-def fonctiontraitementp3(liste):
-    file = "P1-P6\chambers.txt"
-    liste = traitement(file)
+
+"""
+def parametrep1(liste: list) -> tuple:
+    pixels = []
+    px = liste[0]
+    format = liste[1]
+    for i in range(3, len(liste)):
+        pixels.append(liste[i])
+    return (px, format, pixels)
+
+>>>> Fonction a inserer <<<<
+"""
+
+def binaire(liste):
+    liste2 = []
+    for i in range(2, len(liste)):
+        liste2.append(liste[i])
+    return liste2
+
+def p2(liste):
+    """
+    """
+    return [(liste[i], liste[i], liste[i]) for i in range(len(liste))]
+
+if __name__  == "__main__":
+    
+    parser = argparse.ArgumentParser(prog='app1-pb1-106',
+                                    description='Affiche des images PPM')
+    parser.add_argument('fichier', help='Selectionner des images sous format PPM (3, 2 ou 1)')     
+    parser.add_argument('--mise_a_jour', type=bool, default=False,
+                                    help='Affiche image px par px')
+
+    args = vars(parser.parse_args())
+    miseajour = args['mise_a_jour']
+    liste = traitement(args['fichier'])
     liste = totext(liste)
     liste = suppression(liste)
     liste = backslash(liste)
-    if check(file) == "P3":
-        px, format, valmax, pixels = parametrep3(liste)
-        pixels = separation(pixels)
-        pixels = separation2(pixels)
+
+    if check(args['fichier']) == "P3" or check(args['fichier']) == "P2":
+        format, valmax, pixels = parametre(liste, "P3")
+        x, y = forme(format)
+        pixels = separation(pixels)                 #Doit check type de fichier
+        pixels = separation2(pixels)               
         pixels = strtoint(pixels)
-        pixels = totuple(pixels)
+
+        if check(args['fichier']) == "P2":
+            pixels = p2(pixels)
+        else:
+            pixels = totuple(pixels)
         pixels = produit(valmax, pixels)
         pixels = convertion(pixels)
         pixels = addition(pixels)
         pixels = addition2(pixels)
-        x, y = forme(format)
+        if x < 100 and y < 100:
+            cree_fenetre(x*10, y*10)
+            ligne(x, y, pixels, miseajour=miseajour, taille=10)
+        else:
+            cree_fenetre(x, y)
+            ligne(x, y, pixels, miseajour=miseajour)
 
-def fonctionp2(liste, taille):
-    pass
+    if check(args['fichier']) == "P1":
 
+        pixels, format = parametre(liste, 'P1')
+        a, b = liste[0], liste[1]
+        a = b.split(" ")
+        liste2 = []
+        for elem in a:
+            liste2.append(int(elem))
+        x, y = liste2[0], liste2[1]
+        if x <= 50 or y <= 50:
+            cree_fenetre(x*10, y*10)
+        else:
+            cree_fenetre(x, y)
+        pixels = binaire(liste)
+        liste = strtoint1(pixels)
+        ligne1(x, y, liste, miseajour=miseajour)
 
-
-if __name__  == "__main__":
-
-    liste = traitement("P3.txt")  
-    liste = totext(liste)
-    liste = suppression(liste)
-    liste = backslash(liste)
-    format, valmax, pixels = parametrep3(liste)
-    x, y = forme(format)
-    pixels = separation(pixels)                 #Doit check type de fichier
-    pixels = separation2(pixels)               
-    pixels = strtoint(pixels)
-    pixels = totuple(pixels)
-    pixels = produit(valmax, pixels)
-    pixels = convertion(pixels)
-    pixels = addition(pixels)
-    pixels = addition2(pixels)
-    
-    if x < 100 and y < 100:
-        cree_fenetre(x*100, y*100)
-        lignep3(x, y, pixels, 100)
-    else:
-        cree_fenetre(x*10, y*10)
-        lignep3(x, y, pixels, 10)
     attend_fermeture()
---
+++ test.py
@@ -0,0 +1,6 @@
+def p2(liste):
+    """
+    """
+    return [(liste[i], liste[i], liste[i]) for i in range(len(liste))]
+
+print(p2([1,2,5,4,8,7,9,5,'10','5']))
